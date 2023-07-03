5dedb3ccf972486bc9514cb018d8406f66debb5b
Thalia <ecomdragona@gmail.com>
2023-07-03 16:00:24 -0400

    the Default project

---
+++ blog-tutorial/app/routes/logout.jsx
@@ -0,0 +1,7 @@
+import { redirect } from "@remix-run/node";
+
+import { logout } from "~/session.server";
+
+export const action = async ({ request }) => logout(request);
+
+export const loader = async () => redirect("/");
